4358f8b9c5c92211c42e7f22c3dccf980c7fc47d
Kenneth Kiang Jia Ming <kiangjiaming@gmail.com>
2021-02-02 16:41:02 +0800

    set up, learn auth first

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express()
+
+const posts = [
+    {
+        username: 'Kenneth',
+        title: 'Post 1'
+    }, {
+        username: 'Jim',
+        title: 'Post 2'
+    }
+]
+
+app.get('/posts', (req, res) => {
+    res.json(posts)
+})
+
+app.get('/login', (req, res) => {
+    
+})
+
+app.listen(3000)
