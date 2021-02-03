c576cc0654b1de9329855ca82a14db8772a72cd2
Kenneth Kiang Jia Ming <kiangjiaming@gmail.com>
2021-02-03 16:56:21 +0800

    JWT functions, auth server and normal server completed

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express()
+
+const jwt = require('jsonwebtoken')
+
+// use JSON inside of request
+app.use(express.json())
 
 const posts = [
     {
@@ -11,12 +18,21 @@ const posts = [
     }
 ]
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
-app.get('/login', (req, res) => {
-    
-})
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(3000)
